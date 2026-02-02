8fc6ec9555c1ee0350a524b2744839c7049b2cc5
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-02-02 16:45:48 +0100

    Updated Changes

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -7,10 +7,10 @@ use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware('auth:admin');
-    }
+    // public function __construct()
+    // {
+    //     $this->middleware('auth:admin');
+    // }
  
     public function index()
     {
