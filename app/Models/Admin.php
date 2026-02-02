c412f337d436c94ef8123cdd2e30366d606b69dd
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-02-02 15:56:48 +0100

    Updated Changes

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -2,9 +2,33 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
-class Admin extends Model
+class Admin extends Authenticatable
 {
-    //
+    use HasApiTokens, HasFactory, Notifiable;
+
+    protected $connection = 'admin';
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'role',
+        'is_active',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+        'is_active' => 'boolean',
+    ];
 }
--- database/migrations/2026_02_02_145533_create_admins_table.php
+++ database/migrations/2026_02_02_145533_create_admins_table.php
@@ -11,8 +11,15 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('admins', function (Blueprint $table) {
+        Schema::connection('admin')->create('admins', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->enum('role', ['super_admin', 'admin', 'editor'])->default('admin');
+            $table->boolean('is_active')->default(true);
+            $table->rememberToken();
             $table->timestamps();
         });
     }
