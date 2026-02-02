69738d3c412b0f68dffa33f3d94beff77f14dfa7
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-02-02 15:57:48 +0100

    Updated Changes

--- config/auth.php
+++ config/auth.php
@@ -68,6 +68,10 @@ return [
             'driver' => 'eloquent',
             'model' => env('AUTH_MODEL', App\Models\User::class),
         ],
+        'admins' => [
+            'driver' => 'eloquent',
+            'model' => App\Models\Admin::class,
+        ],
 
         // 'users' => [
         //     'driver' => 'database',
