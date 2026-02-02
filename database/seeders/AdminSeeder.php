242ad4cfd75452505220a372fc71f601a8d8d3a6
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-02-02 16:05:48 +0100

    Updated Changes

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\Admin;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class AdminSeeder extends Seeder
 {
@@ -13,6 +14,20 @@ class AdminSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Admin::create([
+            'name' => 'Super Admin',
+            'email' => 'admin@wgrcfp.com',
+            'password' => Hash::make('password'),
+            'role' => 'super_admin',
+            'is_active' => true,
+        ]);
+
+        Admin::create([
+            'name' => 'Regular Admin',
+            'email' => 'editor@example.com',
+            'password' => Hash::make('password'),
+            'role' => 'admin',
+            'is_active' => true,
+        ]);
     }
 }
