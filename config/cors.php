569f9bc42fac64db8366899e144538b6c06ad2c4
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-02-05 14:18:33 +0100

    Updated Changes

---
+++ config/cors.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    
+    'allowed_methods' => ['*'],
+    
+    'allowed_origins' => [
+        'https://wgrcfp.org',
+        'http://localhost:3000',
+        'http://localhost:8000'
+    ],
+    
+    'allowed_origins_patterns' => [],
+    
+    'allowed_headers' => ['*'],
+    
+    'exposed_headers' => [],
+    
+    'max_age' => 0,
+    
+    'supports_credentials' => false,
+];
