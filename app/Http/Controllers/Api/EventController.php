5e1ba95f25e27ae232fc612896bd498f5c7e835e
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-02-06 20:49:05 +0100

    Updated Changes

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -68,8 +68,8 @@ class EventController extends Controller
                 'name' => $speaker->name,
                 'title' => $speaker->title,
                 'brief' => $speaker->brief,
-                'avatar' => $speaker->image ? asset('storage/' . $speaker->image) : asset('storage/' . $speaker->image) ,
-                'image_url' => $speaker->image ? asset('storage/' . $speaker->image) : 'wwww',
+                'avatar' => $speaker->image ? asset('storage/speaker' . $speaker->image) : null,
+                'image_url' => $speaker->image ? asset('storage/speaker' . $speaker->image) : null,
                 'order' => $speaker->order, 
             ];
         })->toArray();
