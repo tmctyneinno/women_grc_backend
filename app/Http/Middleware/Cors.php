1b3d5c9e943cf08cc0bbe212ac23b2e19051fdd0
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-02-06 18:47:03 +0100

    Updated Changes

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -22,12 +22,15 @@ class Cors
         ]);
         
         // ALLOWED ORIGINS - MUST INCLUDE www.wgrcfp.org
+        // $allowedOrigins = [
+        //     'https://www.wgrcfp.org',
+        //     'https://wgrcfp.org',
+        //     'http://localhost:3000',
+        //     'http://127.0.0.1:3000',
+        //     'http://localhost:8000',
+        //     'http://127.0.0.1:8000',
+        // ];
         $allowedOrigins = [
-            'https://www.wgrcfp.org',
-            'https://wgrcfp.org',
-            'http://localhost:3000',
-            'http://127.0.0.1:3000',
-            'http://localhost:8000',
             'http://127.0.0.1:8000',
         ];
         
