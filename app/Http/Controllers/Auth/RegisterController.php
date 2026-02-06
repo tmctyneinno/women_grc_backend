add1b98656766998533fa06ca45a30c27846d76e
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-02-06 23:02:05 +0100

    Updated Changes

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -10,7 +10,7 @@ use Illuminate\Support\Facades\Validator;
 
 class RegisterController extends Controller
 {
-    /*
+    /* 
     |--------------------------------------------------------------------------
     | Register Controller
     |--------------------------------------------------------------------------
