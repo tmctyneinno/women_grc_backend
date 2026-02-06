944c644358201c4d364af444e0fd3d8a2cf69148
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-02-06 23:50:04 +0100

    Updated Changes

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -63,6 +63,7 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
+         \Log::error('Registration success:');
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
