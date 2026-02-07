b2959798b6bd24908d40fb2829d21e2dc2d47b44
Eshanokpe Daniel <eshanokpe@gmail.com>
2026-02-07 20:01:02 +0100

    Updated Changes

--- resources/views/admin/events/edit.blade.php
+++ resources/views/admin/events/edit.blade.php
@@ -347,7 +347,7 @@
                             @if($event->featured_image)
                             <div class="mb-3">
                                 <p class="text-muted small">Current Image:</p>
-                                <img src="{{ asset('storage/' . $event->featured_image) }}" 
+                                <img src="{{ asset('storage/' . $event->featured_image) }}"  
                                      alt="Current featured image" 
                                      class="img-fluid rounded" 
                                      style="max-height: 200px;">
